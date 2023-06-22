968e514db493903e0ec42476ddad91378d37bba3
Juan Cruz Yanez <juanchi097@gmail.com>
2023-06-22 13:57:33 -0300

    Adding the nft-trait contract and the auto-mint-nft contract

--- tests/cbtc-token_test.ts
+++ tests/cbtc-token_test.ts
@@ -1,26 +1,26 @@
 
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.7.0/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.31.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
     name: "Ensure that <...>",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        // arrange: set up the chain, state, and other required elements
-        let wallet_1 = accounts.get("wallet_1")!;
-
-        // act: perform actions related to the current test
         let block = chain.mineBlock([
-            /*
-             * Add transactions with:
+            /* 
+             * Add transactions with: 
              * Tx.contractCall(...)
             */
         ]);
-
-        // assert: review returned data, contract state, and other requirements
         assertEquals(block.receipts.length, 0);
         assertEquals(block.height, 2);
 
-        // TODO
-        assertEquals("TODO", "a complete test");
+        block = chain.mineBlock([
+            /* 
+             * Add transactions with: 
+             * Tx.contractCall(...)
+            */
+        ]);
+        assertEquals(block.receipts.length, 0);
+        assertEquals(block.height, 3);
     },
 });
